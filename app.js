57aa95cecc8b8fb244689f3a6c81a6783aa31572
Akhror Mirfayizev <mi7akhror@gmail.com>
2022-10-23 17:20:15 -0700

    push

---
+++ app.js
@@ -0,0 +1,140 @@
+//expess js
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser =require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const messages = require('express-messages');
+const config = require('./config/database');
+
+
+
+
+//const DB_URI = 'mongodb://rs0:27018,rs1:27019,rs2:27020,rs3:27021,rs4:27022/test';
+mongoose.connect(config.database);
+let db = mongoose.connection;
+
+
+
+//check connection
+db.once('open', function(){
+console.log('Connected to server');
+});
+  db.on('error', function(error){
+    console.log('Your error', error);
+  });
+
+
+
+
+
+
+
+
+//init app
+const app = express();
+
+//  Bring in Models
+let Article = require('./models/article');
+
+
+
+
+//pug
+//load view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+
+
+// Body parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+
+
+// Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Express Session Middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true,
+
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+
+// Express Validation Middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.')
+    , root   = namespace.shift()
+    , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+
+
+// Passport config
+require('./config/passport')(passport);
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.get('*', function(req, res, next){
+  res.locals.user = req.user || null;
+  next();
+});
+
+// home rotue
+app.get('/', function(req, res){
+Article.find({}, function(err, articles){
+  if(err){
+    console.log(err);
+  }else {
+
+
+  res.render('index', {
+    title: 'Hello there',
+    articles: articles
+    });
+  }
+  });
+});
+
+
+// Route files
+let articles = require('./routes/articles');
+let users = require('./routes/users');
+app.use('/articles', articles);
+app.use('/users', users);
+
+
+// start server
+app.listen(3001, function(){
+  console.log('Server started on port 3001....');
+});
---
+++ routes/articles.js
@@ -0,0 +1,152 @@
+const express = require('express');
+const router = express.Router();
+
+//  Bring in Article Models
+let Article = require('../models/article');
+
+//  Bring in Article Models
+let User = require('../models/user');
+
+
+
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// how to add route
+router.get('/add', ensureAuthenticated, function(req, res){
+  res.render('add_article', {
+    title:'Add article'
+  });
+});
+
+
+
+//Add Submit POST Route
+router.post('/add', function(req, res){
+
+req.checkBody('title','Title is required').notEmpty();
+// req.checkBody('author','Author is required').notEmpty();
+req.checkBody('body','Body is required').notEmpty();
+
+// Get Errors
+let errors = req.validationErrors();
+
+if(errors){
+  res.render('add_article', {
+    title:'Add Article',
+    errors:errors
+  });
+} else {
+  console.log('yes , you hacked this');
+    let article = new Article();
+    article.title =req.body.title;
+    article.author =req.user._id;
+    article.body =req.body.body;
+
+    console.log(req.body.body);
+
+  article.save(function(err){
+  if(err){
+    console.log(err);
+  }else{
+    req.flash('success','Article added');
+    res.redirect('/');
+  }
+  })
+
+}
+
+});
+
+// Load Edit form
+router.get('/edit/:id', ensureAuthenticated, function(req, res){
+  Article.findById(req.params.id, function(err, article){
+    if(article.author != req.user._id){
+      req.flash('danger', 'Not Authorized');
+      res.redirect('/');
+    }
+    res.render('edit_article', {
+      tite:'Edit Article',
+      article: article
+    });
+  });
+})
+
+// Update Submit POST Route
+router.post('/edit/:id', function(req, res){
+console.log('yes , you hacked this');
+  let article = {};
+  article.title =req.body.title;
+  article.author =req.body.author;
+  article.body =req.body.body;
+  //onsole.log(req.body.body);
+  let query = {_id:req.params.id}
+
+Article.update(query, article, function(err){
+if(err){
+  console.log(err);
+}else{
+  res.redirect('/');
+}
+
+})
+});
+
+
+
+
+// Delete articles
+router.delete('/:id', function(req, res){
+if(!req.user._id){
+  res.status(500).send();
+}
+
+
+
+  let query = {_id:req.params.id}
+
+  Article.findById(req.params.id, function(err, article){
+    if(article.author != req.user._id){
+      res.status(500).send();
+    } else {
+      Article.remove(query, function(err){
+        if(err){
+          console.log(err);
+        }
+        res.send('Success')
+      });
+    }
+  });
+  });
+
+
+// Get Single article
+router.get('/:id', function(req, res){
+  Article.findById(req.params.id, function(err, article){
+    User.findById(article.author, function(err, user){
+      res.render('article', {
+        article: article,
+        author: user.name
+
+    });
+    });
+  });
+});
+
+
+// Access control
+function ensureAuthenticated(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  } else {
+    req.flash('danger', 'Please login');
+    res.redirect('/users/login');
+  }
+}
+
+module.exports = router;
